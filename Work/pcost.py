18f737b3e8e4ab83b105c2d6f5a1b87858bda13d
AnotherCoolDude <christian.hovenbitzer@gmx.net>
2020-06-24 09:17:11 +0200

    first commit

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,14 @@
 # pcost.py
 #
 # Exercise 1.27
+
+f = open("Data/portfolio.csv", "rt")
+next(f)
+total = 0.0
+for row in f: 
+    data = row.split(",")
+    total += int(data[1]) * float(data[2])
+
+f.close()
+print(f"Total cost {round(total, 2)}")
+
