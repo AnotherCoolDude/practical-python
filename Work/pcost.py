85000245ced690e8e5ca90c13a48adeffc1ae264
Christian Hovenbitzer <christianhovenbitzer@chovenbitzer.fritz.box>
2020-06-24 15:50:56 +0200

    second commit

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,14 +1,22 @@
 # pcost.py
 #
 # Exercise 1.27
+import csv
 
-f = open("Data/portfolio.csv", "rt")
-next(f)
-total = 0.0
-for row in f: 
-    data = row.split(",")
-    total += int(data[1]) * float(data[2])
+def portfolio_cost(filepath):
+    f = open(filepath, "rt")
+    rows = csv.reader(f)
+    header = next(rows)
+    total = 0.0
+    for rowno, row in enumerate(rows): 
+        record = dict(zip(header, row))
+        try:
+            nshares = int(record["shares"])
+            price = float(record["price"])
+            total += nshares * price
+        except ValueError:
+            print(f"Row {rowno}: Bad row: {row}")
 
-f.close()
-print(f"Total cost {round(total, 2)}")
+    f.close()
+    print(f"Total cost {round(total, 2)}")
 
--- Work/report.py
+++ Work/report.py
@@ -8,11 +8,30 @@ def read_portfolio(filename):
 
     with open(filename, "rt") as file:
         rows = csv.reader(file)
-        next(rows)
+        header = next(rows)
         for row in rows:
-            portfolio.append((row[0], int(row[1]), float(row[2])))
+            portfolio.append({header[0]: row[0], header[1]: int(row[1]), header[2]: float(row[2])})
     
     return portfolio
 
 
+def read_prices(filename):
+    f = open(filename, "r")
+    rows = csv.reader(f)
+    stocks = {}
+    for row in rows:
+        if not row:
+            continue
+        stocks[row[0]] = float(row[1])
+    
+    f.close()
+    return stocks
 
+def calculate_stocks(portfolio, prices):
+    portfolio = read_portfolio(portfolio)
+    prices = read_prices(prices)
+    total = 0.0
+    for stock in portfolio:
+        current_price = stock["price"] - prices[stock["name"]]
+        total += current_price * stock["shares"]
+    return total
